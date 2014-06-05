fa38ec744520a6b6261963830400b1df3de7d58d
Jaimin <jaiminpandya@example.com>
2014-06-05 14:06:34 +0530

    Change to more secure remember token

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,14 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
   def feed
     Micropost.from_users_followed_by(self)
   end
@@ -47,8 +55,7 @@ class User < ActiveRecord::Base
   end
 
   private
-  def create_remember_token
-    self.remember_token = SecureRandom.urlsafe_base64
-  end
+   def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
-
